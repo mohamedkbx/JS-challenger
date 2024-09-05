dae75d5876abcb5b3cdce059cf83f6d1654b69bc
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-05 14:52:39 +0300

    feat: Add function to extract keys from a JavaScript object

---
+++ Objects/Extract keys from Javascript object.js
@@ -0,0 +1,10 @@
+// Write a function that takes an object (a) as argument.
+//  Return an array with all object keys.
+
+function myFunction(a) {
+  return Object.keys(a);
+}
+
+// Test Cases:
+
+myFunction({ a: 1, b: 2, c: 3 });
