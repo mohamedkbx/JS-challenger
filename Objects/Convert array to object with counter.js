dcd704cf557ad69cce88a4a3774a17aa2ae85899
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-07 18:21:38 +0300

    feat: Convert array to object with counter function

---
+++ Objects/Convert array to object with counter.js
@@ -0,0 +1,14 @@
+// Convert array to object with counter
+// .Write a function that takes an array of numbers as argument.
+//  Convert the array to an object. It should have a key for each unique value of the array.
+//   The corresponding object value should be the number of times the key occurs within the array.
+function myFunction(arr) {
+  const result = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+  }, {});
+
+  return result;
+}
+myFunction([1, 2, 2, 3, 3, 3]);
+// Expected output: { 1: 1, 2: 2, 3: 3 }
