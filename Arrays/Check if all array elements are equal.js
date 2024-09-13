9e3b805bbbd5e8432f73306f63233c98a132309f
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-13 23:50:12 +0300

    feat: Add function to check if all array elements are equal

---
+++ Arrays/Check if all array elements are equal.js
@@ -0,0 +1,3 @@
+// Check if all array elements are equal
+// Write a function that takes an array as argument.
+//  It should return true if all elements in the array are equal. It should return false otherwise.
