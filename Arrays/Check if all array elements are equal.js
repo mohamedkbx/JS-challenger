59a80974ac7ca2bf086c8608123d072f5b1e6c2b
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-13 23:54:13 +0300

    refactor :add solution to check array elements

--- Arrays/Check if all array elements are equal.js
+++ Arrays/Check if all array elements are equal.js
@@ -1,3 +1,7 @@
 // Check if all array elements are equal
 // Write a function that takes an array as argument.
 //  It should return true if all elements in the array are equal. It should return false otherwise.
+
+function myFunction(arr) {
+  return arr.every((val, i, arr) => val === arr[0]);
+}
