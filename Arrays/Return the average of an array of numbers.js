6c532b0521fcd33f7405171634527e363e577bed
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-13 23:04:50 +0300

    feat: Add function to calculate the average of an array of numbers

---
+++ Arrays/Return the average of an array of numbers.js
@@ -0,0 +1,7 @@
+// Return the average of an array of numbers
+// Write a function that takes an array of numbers as argument.
+//  It should return the average of the numbers.
+
+function myFunction(arr) {
+  return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+}
